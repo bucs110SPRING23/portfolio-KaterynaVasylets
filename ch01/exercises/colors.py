d0979c1e69711019698d2c1dc7829c8f7d67325a
Kateryna Vasylets <kvasyle1@binghamton.edu>
2023-02-01 17:49:59 -0500

    colors.py

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,13 @@
+colors=[]
+"green"
+"blue"
+"purple"
+"orange"
+"yellow"
+print("I have",len(mycolors),"colors")
+print(mycolors[0])
+print(mycolors[1])
+print(mycolors[2])
+print(mycolors[3])
+print(mycolors[4])
+print(mycolors[5])
