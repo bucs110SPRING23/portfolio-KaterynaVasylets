55d3994f6c20059b8e3a0c2584d935701bc8b565
Kateryna Vasylets <kvasyle1@binghamton.edu>
2023-02-15 11:28:28 -0500

    main.py cho2/lab

--- ch02/shapes.py
+++ ch02/shapes.py
@@ -1,6 +1,23 @@
-pygame = pygane. display. set_mode ()
-screen. fill ("green")
-pygame. display. flip ()
-pygame. time. wait(1000)
-screen_size= screen. get_size()
-dimensions=[screen_size[0]/2]
+import pygame
+pygame. init()
+while 1:
+    
+screen = pygame.display.set_mode()
+screen. fill("blue")
+pygame.display. flip()
+pygame.time.wait(1000)
+screen_size = screen.get_size()
+#[x, y, width, height]
+dimensions = [screen_size[0] / 2, screen_size[1] / 2, 250, 250]
+pygame.draw.rect(screen, "red", dimensions)
+#[x, y, width, height]
+dimensions = [300, 300, 250, 250]
+pygame.draw.rect(screen, "green", dimensions)
+pygame. display. flip()
+input()
+
+
+
+
+
+
