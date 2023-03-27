17219ec84af89b85cf516a34c227f00d519225d9
Kateryna Vasylets <kvasyle1@binghamton.edu>
2023-03-27 14:07:01 -0400

    fixed ch4 lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,30 +1,41 @@
 import pygame
 import random
 import math
-# PART A
-pygame.init()
+while 1:
+    # PART A
+    pygame.init()
 
-screen_width=500
-screen_height=500
-screen = pygame.display.set_mode([screen_width, screen_height])
+    screen_width=500
+    screen_height=500
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    screen.fill("black")
 
+    pygame.draw.circle(screen, (0, 0, 0), (250,250), 250, width=3)
+    pygame.draw.line(screen, "white", (500,-250), (500,250), width=3)
+    pygame.draw.line(screen, "white", (0,250), (500,250), width=3)
+    pygame.display.flip()
 
-pygame.draw.circle(screen, (0, 0, 0), (250,250), 250, width=3)
-pygame.draw.line(screen, "black", (500,-250), (500,250), width=3)
-pygame.draw.line(screen, "black", (0,250), (500,250), width=3)
-pygame.display.flip()
+    #part B
 
-#part B
+    for darts in range(10):
+        dart1x = random.randrange(0,500)
+        dart1y = random.randrange(0,500)
+        dart2x = random.randrange(0,500)
+        dart2y = random.randrange(0,500)
+        distance_from_center1 = math.hypot(dart1x-250, dart1y-250)
+        distance_from_center2 = math.hypot(dart2x-250, dart2y-250)
+        is_in_circle1 = (distance_from_center1 <= 250)
+        is_in_circle2 = (distance_from_center2 <= 250)
+        if  is_in_circle1 < 500:
+            pygame.draw.circle(screen, "blue", [dart1x,dart1y], 10)
+        else:
+            pygame.draw.circle(screen, "green", [dart1x,dart1y], 10) 
+            print("wrong", distance_from_center1)
 
-for darts in range(10):
-    x = random.randrange(0,500)
-    y = random.randrange(0,500)
-    distance_from_center = math.hypot(x-250, y-250)
-    is_in_circle = distance_from_center 
-    if  is_in_circle < 500:
-        pygame.draw.circle(screen, "blue", [x,y], 10)
-    else:
-        pygame.draw.circle(screen, "green", [x,y], 10) 
-        print("wrong", distance_from_center)
-pygame.display.flip()
-pygame.time.wait()
+        if  is_in_circle2 < 500:
+            pygame.draw.circle(screen, "blue", [dart2x,dart2y], 10)
+        else:
+            pygame.draw.circle(screen, "green", [dart2x,dart2y], 10) 
+            print("wrong", distance_from_center2)
+    pygame.display.flip()
+    pygame.time.wait(5000)
--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,13 +1,13 @@
 import json
 
 def main():
-    text = open("news.txt", "r").read()
+    news = open("news.txt", "r").read()
     sub_fptr = open("subs.json", "r") 
     subs = json.load(sub_fptr)
     print(subs, type(subs))
 
     for k,v in subs.items():
-        text = text.replace(k,v)
+        news = text.replace(k,v)
 
     fptr = open("betternews.txt", "w")
     fptr.write(text)
