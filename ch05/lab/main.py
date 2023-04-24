2ac8917dfae8bc62876892f23a712d32dab4a514
KaterynaVasylets <123112291+KaterynaVasylets@users.noreply.github.com>
2023-04-24 13:55:19 -0400

    Update main.py

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -9,39 +9,47 @@ def threenp1(n):
             n = int(n / 2)
         else:
             n = int(3*n+1)
-            while n == 1.0:
-                return count
-            
+    return count
+        
 
 def threenp1range(upper_limit):
     threenp1_dict = {}
     for i in range(2, upper_limit + 1):
-        count = calculate_threenp1(i)
+        count = threenp1(i)
         threenp1_dict[i] = count
     return threenp1_dict
 
 def graph_coordinates(threenplus1_iters_dict):
 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((1100, 700))
     
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-        
-        pygame.draw.lines(screen, (0, 0, 255), False, list(threenplus1_iters_dict()))
-        
-        max_count = max(threenplus1_iters_dict())
-        max_msg = f"Max count so far is: {max_count}"
-        font = pygame.font.Font(None, 48)
-        max_text = font.render(max_msg, True, (0, 0, 255))
-        
-        screen.blit(max_text, (800, 20))
-        pygame.display.flip()
+    # while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            return
     
-    pygame.time.wait(6000)
+    pygame.draw.lines(screen, (0, 0, 255), False, list(threenplus1_iters_dict.items()))
+    screen.blit(screen, (0,0))
+
+    width, height = screen.get_size()
+    new_screen = pygame.transform.scale(screen, (width * 5, height * 5))
+    screen.blit(new_screen, (0,0))
+
+    new_screen = pygame.transform.flip(screen, False, True)
+    screen.blit(new_screen, (0,0))
+    
+
+    max_count = max(threenplus1_iters_dict)
+    max_msg = f"Max count so far is: {max_count}"
+    font = pygame.font.Font(None, 48)
+    max_text = font.render(max_msg, True, (0, 0, 255))
+    
+    screen.blit(max_text, (400, 20))
+    pygame.display.flip()
+
+    pygame.time.wait(3000)
 
 
 
