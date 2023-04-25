c419a4cde356b8472a3d842c499747d488ded835
Kateryna Vasylets <kvasyle1@binghamton.edu>
2023-04-24 23:15:31 -0400

    yes

---
+++ ch08/lab/stringutility.py
@@ -0,0 +1,39 @@
+class StringUtility:
+    def __init__(self, input_str):
+        self.str = input_str
+
+    def vowels(self):
+        vowel_count = sum(1 for c in self.str if c.lower() in 'aeiou')
+        if vowel_count < 5:
+            return str(vowel_count)
+        else:
+            return 'many'
+
+    def bothEnds(self):
+        if len(self.str) <= 2:
+            return ''
+        else:
+            return self.str[:2] + self.str[-2:]
+
+    def fixStart(self):
+        if len(self.str) <= 1:
+            return self.str
+        else:
+            first_char = self.str[0]
+            return first_char + self.str[1:].replace(first_char, '*')
+
+    def asciiSum(self):
+        return sum(ord(c) for c in self.str)
+
+    def cipher(self):
+        shift = len(self.str)
+        result = ''
+        for c in self.str:
+            if c.isalpha():
+                if c.islower():
+                    result += chr((ord(c) - ord('a') + shift) % 26 + ord('a'))
+                else:
+                    result += chr((ord(c) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                result += c
+        return result
