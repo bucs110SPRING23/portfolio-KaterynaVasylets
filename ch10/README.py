9cf3c4118449e155285741657aff89663e769d22
Kateryna Vasylets <kvasyle1@binghamton.edu>
2023-05-10 21:29:57 -0400

    ;)

---
+++ ch10/README.py
@@ -0,0 +1,10 @@
+# CS110 Final Exam
+
+## SHORT DESCRIPTION *(1 or 2 sentences describing your program)*
+You ask the user what are their favorite genres of music is and how many recommendations, and the output uses API to get recommendations from Spotify.
+
+## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
+
+## REFERENCES *(any resources you use outside of class materials)*
+
+## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
